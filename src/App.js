e98fee8e03d486fd6453c79e157ecacedbe60226
yyogesh <yogeshggn1@gmail.com>
2020-10-03 12:12:10 +0530

    React useRef hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('')
+  const [counter, setCounter] = useState(0); // 1
+  const previousCounterRef = useRef(); // 2
+  const renderCount = useRef(1);
+
+  const inputRef = useRef();
+
+
+  useEffect(() => {
+    previousCounterRef.current = counter; // 4
+    renderCount.current = renderCount.current + 1;
+    inputRef.current.focus();
+  }, [counter])
+
+  // 3
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count : {counter} </h1>
+      {
+        typeof previousCounterRef.current !== 'undefined' &&
+        <h2>Previous : {previousCounterRef.current}</h2>
+      }
+      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={() => {
+        setCounter(Math.ceil(Math.random() * 10)) // 5
+      }}>
+        Click Me
+       </button>
+
+      <h4>
+        {renderCount.current}
+      </h4>
     </div>
   );
 }
